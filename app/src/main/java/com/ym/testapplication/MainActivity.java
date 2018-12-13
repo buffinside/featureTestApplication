13c6a861e623cc988bc0c387e2e0de413107e804
尹传友 <1468156632@qq.com>
2018-12-13 22:01:26 +0800

    测试了一下hashmap以及捕获异常的一些特性。 以及新增拷贝工程目录的方法，为一键发布ymlib做准备 TODO:自定义插件，一个可执行的jar包，接收指令packer create android_app_template -path <project path> -game_name <exterior_game_name> -libs <libs list> 于指定的目录（缺省本目录）创建项目根目录名为exterior_game_name 包名指代部分的目录，并下载相应的构建参数、期望集成的库模块的一个游戏工程 在一键发布工具的构建思维之下，创建一个ym的库工程是没有必要的。 好好想想一个测试的库APP工程，如何一键发布成符合ymsdk对接库工程的模块。 ①继承YMApplication。避免使用application的this对象的时候有不匹配的情况。 ②通过注解标记，自动注册回调代码，相当于autowired的功能，从而达到一键发布，一键集成。 ③com.android.application→com.android.library; AndroidManifest.xml中相关的android:name给去掉（一个应用里面只允许一个android:name，不能有多个，所以库里面的得去掉）

--- app/src/main/java/com/ym/testapplication/MainActivity.java
+++ app/src/main/java/com/ym/testapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.HashMap;
+
 import static com.ym.testapplication.SystemUtil.getUserInfo;
 
 public class MainActivity extends Activity {
@@ -19,6 +21,19 @@ public class MainActivity extends Activity {
         int versionCode = BuildConfig.VERSION_CODE;
         String versionName = BuildConfig.VERSION_NAME;
         Log.e(TAG, "onCreate: versionCode: " + versionCode + " versionName: " + versionName );
+
+        Integer test = null;
+        try{
+        test.toString();
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+        HashMap map = new HashMap();
+        map.put("name", "value");
+        map.put("name", null);
+        Log.e(TAG, "onCreate: " + String.valueOf(map.get("name")));
+
     }
 
 
